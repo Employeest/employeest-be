677226e51c1dde120edb1d016f1855496d0a2605
datsiuk-ira <shevchenkoari@gmail.com>
2025-05-27 15:25:50 +0300

    Extended Team, Project, Task

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_nested import routers
 
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token  # For login
+
 from .views import (
     ProjectViewSet,
     TaskViewSet,
@@ -13,9 +13,12 @@ from .views import (
     OwnerDashboardView,
     EmployeeDashboardView,
     UserProfileView,
-    UserRegistrationAPIView,  # <-- Import new registration view
+    UserRegistrationAPIView,
     LogoutAPIView,
-    UserListViewSet
+    UserListViewSet,
+    TeamViewSet,
+    TeamMemberViewSet,
+    TaskCommentViewSet
 )
 
 router = DefaultRouter()
@@ -23,15 +26,21 @@ router.register(r'projects', ProjectViewSet, basename='project')
 router.register(r'tasks', TaskViewSet, basename='task')
 router.register(r'worklogs', WorkLogViewSet, basename='worklog')
 router.register(r'users', UserListViewSet, basename='userlist')
-# If you want a /users endpoint for listing (admin) or retrieving users:
-# from .views import UserViewSet # You would need to create this
-# router.register(r'users', UserViewSet, basename='user')
+router.register(r'teams', TeamViewSet, basename='team')
+
+
+tasks_router = routers.NestedSimpleRouter(router, r'tasks', lookup='task')
+tasks_router.register(r'comments', TaskCommentViewSet, basename='task-comments')
+
+teams_router = routers.NestedSimpleRouter(router, r'teams', lookup='team')
+teams_router.register(r'members', TeamMemberViewSet, basename='team-members')
 
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(tasks_router.urls)),
+    path('', include(teams_router.urls)),
 
-    # Auth
     path('auth/register/', UserRegistrationAPIView.as_view(), name='auth-register'),
     path('auth/login/', obtain_auth_token, name='auth-login'),
     path('auth/logout/', LogoutAPIView.as_view(), name='auth-logout'),
